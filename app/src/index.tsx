580f1f70041965e64f54b4d2a296a7ff3d5a89e1
rg872 <gilang0293@gmail.com>
2022-02-14 22:52:29 +0700

    add more typescript intergration

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,22 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import i18n from "I18n/i18n.config";
+import i18nConfig from "I18n/i18n.config";
 import { I18nextProvider } from "react-i18next";
 import Root from "Core/root";
 import store, { history } from "Redux/store/store";
+
 import "bulma/css/bulma.css";
 
+import { IApi } from "Types/api";
+
+declare global {
+  interface Window {
+    api: IApi;
+  }
+}
+
 ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
+  <I18nextProvider i18n={i18nConfig}>
     <Suspense fallback="loading">
       <Root store={store} history={history}></Root>
     </Suspense>
