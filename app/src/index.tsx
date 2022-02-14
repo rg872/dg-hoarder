05ec53c0095ef390f782354b7c45b6d78228214e
rg872 <gilang0293@gmail.com>
2022-02-14 23:08:32 +0700

    chore: clean src, make it basic

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,9 +1,5 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import i18nConfig from "I18n/i18n.config";
-import { I18nextProvider } from "react-i18next";
-import Root from "Core/root";
-import store, { history } from "Redux/store/store";
 
 import "bulma/css/bulma.css";
 
@@ -15,11 +11,4 @@ declare global {
   }
 }
 
-ReactDOM.render(
-  <I18nextProvider i18n={i18nConfig}>
-    <Suspense fallback="loading">
-      <Root store={store} history={history}></Root>
-    </Suspense>
-  </I18nextProvider>,
-  document.getElementById("target")
-);
+ReactDOM.render(<div>HELLO</div>, document.getElementById("target"));
